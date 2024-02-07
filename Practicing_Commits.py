29d437107a34ca68bb3234e858da34099f2b919b
Ashna Singh <ashnasingh2002@gmail.com>
2024-02-06 19:03:48 -0500

    new file

---
+++ Practicing_Commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 30 11:05:38 2024
+
+@author: Ashna
+"""
+
+x = 7
+y = 23
+z = 9
+print((x + y)/z)
+
+a = "Hello "
+b = "my name is "
+c = "Ashna."
+
+print(a + b + c)
+
+import numpy as np
+np.zeros((3, 7))
